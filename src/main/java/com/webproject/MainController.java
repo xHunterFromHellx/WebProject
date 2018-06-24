cf2aa469eebb9e127bd6f6de70880e5e4ff436fb
Vlad Suprun <vlad.suprun.bsp@gmail.com>
2018-06-24 21:30:14 +0300

    MVC-2

--- src/main/java/com/webproject/MainController.java
+++ src/main/java/com/webproject/MainController.java
@@ -1,4 +1,14 @@
 package com.webproject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String view(){
+        return "index";
+    }
+
 }
