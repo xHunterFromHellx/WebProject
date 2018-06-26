cf86b6f3cd2207a4fd2201eb31e2915a8a7d99ec
Vlad Suprun <vlad.suprun.bsp@gmail.com>
2018-06-26 23:45:10 +0300

    MVC-3

--- src/main/java/com/webproject/MainController.java
+++ src/main/java/com/webproject/MainController.java
@@ -1,13 +1,16 @@
 package com.webproject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String view(){
+    @GetMapping("/view")
+    public String view(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name, Model model){
+        model.addAttribute("msg", "Hello " + name);
         return "index";
     }
 
